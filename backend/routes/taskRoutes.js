15ac60076a7bcf4d17d6ab47e7a110fbd0ede130
NishaadG <nishaadgangal@gmail.com>
2025-07-05 01:03:33 +0530

    first commit

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { protect, adminOnly } = require("../middlewares/authMiddleware");
+const {
+  getDashboardData,
+  getUserDashboardData,
+  getTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+  updateTaskStatus,
+  updateTaskChecklist,
+} = require("../controller/taskController");
+
+const router = express.Router();
+
+// Dashboard Data
+router.get("/dashboard-data", protect, getDashboardData);
+router.get("/user-dashboard-data", protect, getUserDashboardData);
+
+// Task Routes
+router.get("/", protect, getTasks); // Admin: All, User: Assigned
+router.get("/:id", protect, getTaskById);
+router.post("/", protect, adminOnly, createTask);
+router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, adminOnly, deleteTask);
+router.put("/:id/status", protect, updateTaskStatus);
+router.put("/:id/todo", protect, updateTaskChecklist);
+
+module.exports = router;
